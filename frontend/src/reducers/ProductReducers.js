19a1c9c9f05cb43503b8e429967f81d99884574e
jynyigt <jiyanyiit@yandex.com>
2021-03-11 13:54:11 +0300

    Add Redux to Product Screen and Handle Add To Cart

--- frontend/src/reducers/ProductReducers.js
+++ frontend/src/reducers/ProductReducers.js
@@ -1,14 +1,21 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import { PRODUCT_DETAİLS_FAIL, PRODUCT_DETAİLS_REQUEST, PRODUCT_DETAİLS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
 
-export const productListReducer=(state={products:[]},action)=>{
-    switch(action.type){
+export const productListReducer = (state = {}, action) => {
+    switch (action.type) {
         case PRODUCT_LIST_REQUEST:
-            return {loading:true};
-            case PRODUCT_LIST_SUCCESS:
-                return {loading:false,products:action.paylaod};
-                case PRODUCT_LIST_FAIL:
-                    return {loading:false,error:action.paylaod};
-            default:
-                return state;
+            return {
+                products: action.payload
+            }
+        default:
+            return state
+    }
+};
+
+export const productDetailsReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAİLS_REQUEST:
+            return { products: action.payload };
+        default:
+            return state;
     }
 }
